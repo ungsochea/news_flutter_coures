1a51fb634daa9a2e4d7c7576546505a22b5c07b2
“ungsochea” <sochea.ung@gmail.com>
2023-07-12 05:39:01 +0700

    model bottom sheet

--- lib/inner_screens/news_detail.dart
+++ lib/inner_screens/news_detail.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_iconly/flutter_iconly.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:news_flutter_course/services/utils.dart';
+import 'package:news_flutter_course/widgets/vertical_spacing.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class NewDetilScreen extends StatefulWidget {
@@ -38,8 +40,18 @@ class _NewDetilScreenState extends State<NewDetilScreen> {
             style: TextStyle(color: color),
           ),
           actions: [
-            IconButton(onPressed: () {}, icon: const Icon(Icons.more_horiz))
+            IconButton(
+                onPressed: () async {
+                  await _showModelSheetFct();
+                },
+                icon: const Icon(Icons.more_horiz))
           ],
+          leading: IconButton(
+            icon: const Icon(IconlyLight.arrowLeft2),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
         ),
         body: WebView(
           initialUrl:
@@ -56,5 +68,57 @@ class _NewDetilScreenState extends State<NewDetilScreen> {
         ),
       ),
     );
+  }
+
+  Future<void> _showModelSheetFct() async {
+    await showModalBottomSheet(
+        shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.vertical(top: Radius.circular(20))),
+        context: context,
+        builder: (context) {
+          return Container(
+            decoration: BoxDecoration(
+                color: Theme.of(context).cardColor,
+                borderRadius:
+                    const BorderRadius.vertical(top: Radius.circular(20))),
+            child: Column(mainAxisSize: MainAxisSize.min, children: [
+              const VerticalSpacing(20),
+              Center(
+                child: Container(
+                  height: 5,
+                  width: 35,
+                  decoration: BoxDecoration(
+                      color: Colors.grey,
+                      borderRadius: BorderRadius.circular(30)),
+                ),
+              ),
+              const VerticalSpacing(20),
+              const Text(
+                "More Option",
+                style: TextStyle(fontWeight: FontWeight.w600, fontSize: 20),
+              ),
+              const VerticalSpacing(20),
+              const Divider(
+                thickness: 2,
+              ),
+              const VerticalSpacing(20),
+              ListTile(
+                leading: const Icon(Icons.share),
+                title: const Text("Share"),
+                onTap: () {},
+              ),
+              ListTile(
+                leading: const Icon(Icons.open_in_browser),
+                title: const Text("Open in browser"),
+                onTap: () {},
+              ),
+              ListTile(
+                leading: const Icon(Icons.refresh),
+                title: const Text("Refresh"),
+                onTap: () {},
+              ),
+            ]),
+          );
+        });
   }
 }
